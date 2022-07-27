d70c3c134b6a384e40cb8db677aafef7b0132e16
anidlemind <anidlemind001@gmail.com>
2022-07-27 12:13:12 +0300

    Slime's Remorse (No Sprite, Balance, Drop)

--- Items/BasicSword.cs
+++ Items/BasicSword.cs
@@ -1,6 +1,8 @@
-using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
 using TestRepos;
 
 namespace TestRepos.Items
@@ -9,13 +11,12 @@ namespace TestRepos.Items
 	{
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("OP Sword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("This is a basic modded sword.");
+			DisplayName.SetDefault("Slime's Remorse"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
 		}
 
 		public override void SetDefaults()
 		{
-			Item.damage = 50000;
+			Item.damage = 50;
 			Item.DamageType = DamageClass.Melee;
 			Item.width = 100;
 			Item.height = 100;
@@ -28,7 +29,19 @@ namespace TestRepos.Items
 			Item.UseSound = SoundID.Item1;
 			Item.autoReuse = true;
 			Item.shoot = ModContent.ProjectileType<Projectiles.BasicProjectile>();
+			Item.shootSpeed = 10.5f;
 		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+			Vector2 offset = new Vector2(velocity.X * 2, velocity.Y * 2);
+			position += offset;
+			for (int i = -1; i < 2; i++){
+				Vector2 newspd = velocity.RotatedBy(MathHelper.ToRadians(10*i));
+				Projectile.NewProjectile(Projectile.GetSource_NaturalSpawn(), position, newspd, type, 15, knockback, player.whoAmI);
+			}
+			return false;
+        }
 
 		public override void AddRecipes()
 		{
--- Projectiles/BasicProjectile.cs
+++ Projectiles/BasicProjectile.cs
@@ -1,23 +1,46 @@
-using Terraria;
 using Terraria.ID;
+using Terraria;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using System;
 
 namespace TestRepos.Projectiles
 {
 	public class BasicProjectile : ModProjectile
 	{
-		public override void SetDefaults()
+		public override void SetStaticDefaults() 
 		{
-			Projectile.damage = 50;
-			Projectile.DamageType = DamageClass.Melee;
-			Projectile.width = 40;
-			Projectile.height = 40;
-			Projectile.knockBack = 6;
-			Projectile.aiStyle = 1;
-			Projectile.timeLeft = 400;
-			Projectile.ignoreWater = true;
-			Projectile.tileCollide = false;
-			
+			DisplayName.SetDefault("Basic Projectile"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
 		}
-	}
+
+		public override void SetDefaults() 
+		{
+			Projectile.DamageType = DamageClass.Melee;
+			Projectile.damage = 15;
+			Projectile.scale = 0.75f;
+			Projectile.width = 8;
+			Projectile.height = 8;
+			Projectile.aiStyle = 0;
+			Projectile.friendly = true;
+			Projectile.hostile = false;
+			Projectile.penetrate = 2;
+			Projectile.timeLeft = 50;
+			Projectile.light = 0.25f;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = true;
+		}
+
+        public override void AI() //Копипаст от Riptide
+        {
+			int dust = Dust.NewDust(Projectile.Center, 1, 1, 15, 0f, 0f, 0, default(Color), 1f);
+			Main.dust[dust].velocity *= 0.3f;
+			Main.dust[dust].scale = (float)Main.rand.Next(100, 135) * 0.013f;
+			Main.dust[dust].noGravity = true;
+
+			int dust2 = Dust.NewDust(Projectile.Center, 1, 1, 137, 0f, 0f, 0, default(Color), 1f);
+			Main.dust[dust2].velocity *= 0.3f;
+			Main.dust[dust2].scale = (float)Main.rand.Next(80, 115) * 0.013f;
+			Main.dust[dust2].noGravity = true;
+		} 
+    }
 }
